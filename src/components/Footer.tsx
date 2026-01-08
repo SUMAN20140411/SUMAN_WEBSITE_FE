754d57d978bf4c23e273ffeca1f1164a16cdb116
edeniasc <edenia9dspensa@gmail.com>
2026-01-08 22:53:28 +0900

    Update footer contact information for clarity and completeness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,15 +35,15 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto px-6 py-6 text-xs text-gray-700 flex flex-col sm:flex-row justify-between items-center gap-4 tracking-wide">
         {/* Kiri: Informasi kontak */}
         <div>
-          <p>
+          <p> 
             {lang === "KOR"
-              ? "㈜ SUMAN | 주소: 대전광역시 대덕구 문평서로17번길 105(문평동) | 사업자등록번호: 318-81-00161"
-              : "SUMAN Co., Ltd. | Address: 105, Munpyeongseo-ro 17beon-gil, Daedeok-gu, Daejeon, Republic of Korea | Biz Reg No.: 318-81-00161"}
+              ? "㈜ SUMAN | 본사: 대전광역시 대덕구 문평서로 17번길 105(문평동) | 천안사업장: 충남 천안시 서북구 2공단 4로 40-11(성성동) G1 비즈캠퍼스 401호"
+              : "SUMAN Co., Ltd. | HQ: 105, Munpyeongseo-ro 17beon-gil, Daedeok-gu, Daejeon | Cheonan: 40-11, 2gongdan 4-ro, Seobuk-gu, Cheonan-si "}
           </p>
           <p>
             {lang === "KOR"
-              ? "대표전화 : 042-934-1517 | FAX : 042-934-1516 | E-Mail : suman20140411@suman.co.kr"
-              : "Tel: +82-42-934-1517 | Fax: +82-42-934-1516 | Email: suman20140411@suman.co.kr"}
+              ? "| 시험센터: 대전광역시 유성구 테크노 2로 309-26(탑립동 929-1) | 대표전화 : 042-934-1517 | FAX : 042-934-1516 | E-Mail : suman20140411@suman.co.kr"
+              : "| Test Center: 309-26, Techno 2-ro, Yuseong-gu, Daejeon | Tel: +82-42-934-1517 | Fax: +82-42-934-1516 | Email: suman20140411@suman.co.kr"}
           </p>
         </div>
 
