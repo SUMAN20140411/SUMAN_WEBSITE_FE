d4c601c17db0128f9e13a51d3f7bf5a3b02bec66
gideonfrans22 <gideonfrans22>
2026-03-10 11:10:05 +0900

    refactor: remove unused singleType exports from Strapi integration

--- src/lib/strapi/singleTypes.ts
+++ src/lib/strapi/singleTypes.ts
@@ -1,12 +1,5 @@
 import strapiClient from "../api/strapiClient";
 
-export const ceoMessage = strapiClient.single("ceoMessage");
-export const vision = strapiClient.single("vision");
-export const history = strapiClient.single("history");
-export const org = strapiClient.single("org");
-export const ci = strapiClient.single("ci");
-export const location = strapiClient.single("location");
-export const certifications = strapiClient.single("certifications");
 export const service = strapiClient.single("service");
 export const product = strapiClient.single("product");
 export const rnd = strapiClient.single("rnd");
