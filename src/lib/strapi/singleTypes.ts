5467ac463fb14ccef626698e2a9a5c24b87e2f74
gideonfrans22 <gideonfrans22>
2026-03-11 16:34:22 +0900

    chore: remove singleTypes.ts file as it is no longer needed for Strapi integration

--- src/lib/strapi/singleTypes.ts
+++
@@ -1,6 +0,0 @@
-import strapiClient from "../api/strapiClient";
-
-export const service = strapiClient.single("service");
-export const product = strapiClient.single("product");
-export const rnd = strapiClient.single("rnd");
-export const philosophy = strapiClient.single("philosophy");
