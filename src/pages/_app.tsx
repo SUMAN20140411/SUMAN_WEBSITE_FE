60f4b4362215c1bc193c800a8b32f42f9c98a78e
gideonfrans22 <gideonfrans22>
2026-03-13 11:53:22 +0900

    feat: add language detection based on route changes and improve code formatting in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,36 +1,48 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Script from 'next/script';
-import Head from 'next/head';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import Head from "next/head";
+import { useLangStore } from "@/stores/langStore";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
+  const { setLang } = useLangStore();
 
   const handleRouteChange = (url: string) => {
-    if (typeof window.gtag !== 'undefined') {
-      window.gtag('config', 'G-HMD20SMTK5', {
-        page_path: url,
+    if (typeof window.gtag !== "undefined") {
+      window.gtag("config", "G-HMD20SMTK5", {
+        page_path: url
       });
     }
   };
 
   useEffect(() => {
-    router.events.on('routeChangeComplete', handleRouteChange);
+    router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off("routeChangeComplete", handleRouteChange);
     };
   }, [router.events]);
 
+  useEffect(() => {
+    router.pathname.includes("/eng") ? setLang("ENG") : setLang("KOR");
+  }, [router.pathname]);
+
   return (
     <>
-    <Head>
-      {/* google & naver 사이트 확인 태그 (임의 변경 금지) */}
-      <meta name="google-site-verification" content="iT2-OO3Wat9zt_bq-t7Y0F24HWiIyWTz_OsvEyvLd9c" />
-      <meta name="naver-site-verification" content="b90c2478a5b6431a748fa0e68d931f04dc9e4fa9" />
-      <link rel="icon" sizes="16x16" href="/images/logo.ico" />
-    </Head>
+      <Head>
+        {/* google & naver 사이트 확인 태그 (임의 변경 금지) */}
+        <meta
+          name="google-site-verification"
+          content="iT2-OO3Wat9zt_bq-t7Y0F24HWiIyWTz_OsvEyvLd9c"
+        />
+        <meta
+          name="naver-site-verification"
+          content="b90c2478a5b6431a748fa0e68d931f04dc9e4fa9"
+        />
+        <link rel="icon" sizes="16x16" href="/images/logo.ico" />
+      </Head>
       {/* GA4 스크립트 삽입 */}
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=G-HMD20SMTK5`}
